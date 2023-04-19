c981da12a90c5e470b776e25ae4a70bbefe57d55
b1urrrr <b1urrr@naver.com>
2023-04-19 23:14:18 +0900

    [ADD/#1] Birthday Greeting 메시지 글꼴 크기 변경

--- week1/practice/BirthdayCard/app/src/main/java/com/example/bithdaycard/MainActivity.kt
+++ week1/practice/BirthdayCard/app/src/main/java/com/example/bithdaycard/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.bithdaycard.ui.theme.BithdayCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthdayGreetingWithText(modifier: Modifier = Modifier, message: String) {
-    Text(text = message)
+    Text(text = message, fontSize = 36.sp)
 }
 
 @Preview(showBackground = true)
