bb9f4f691ee3630db12221ae81a3c74829ed06a8
b1urrrr <b1urrr@naver.com>
2023-04-20 02:07:09 +0900

    [ADD/#1] Roll 버튼 추가

--- week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -34,4 +38,12 @@ fun DiceWithButtonAndImage(
         .fillMaxSize()
         .wrapContentSize(Alignment.Center),
 ) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.main_roll))
+        }
+    }
 }
