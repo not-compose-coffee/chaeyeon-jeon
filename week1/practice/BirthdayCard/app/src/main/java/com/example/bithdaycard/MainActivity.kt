7c280e6c624d2846b1a521f9b29c3c9fff5d2ba4
b1urrrr <b1urrr@naver.com>
2023-04-20 00:08:33 +0900

    [ADD/#1] 생일 카드 이미지 추가

--- week1/practice/BirthdayCard/app/src/main/java/com/example/bithdaycard/MainActivity.kt
+++ week1/practice/BirthdayCard/app/src/main/java/com/example/bithdaycard/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.bithdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.bithdaycard.ui.theme.BithdayCardTheme
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    BirthdayGreetingWithText(message = "생일 추카해", from = "광렬")
+                    BirthdayGreetingWithImage(message = "생일 추카해", from = "광렬")
                 }
             }
         }
@@ -46,10 +47,19 @@ fun BirthdayGreetingWithText(modifier: Modifier = Modifier, message: String, fro
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(modifier: Modifier = Modifier, message: String, from: String) {
+    val birthdayImage = painterResource(id = R.drawable.img_main_birthday_card)
+    Image(
+        painter = birthdayImage,
+        contentDescription = null,
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BithdayCardTheme {
-        BirthdayGreetingWithText(message = "생신 축하", from = "광렬")
+        BirthdayGreetingWithImage(message = "생신 축하", from = "광렬")
     }
 }
