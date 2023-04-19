c59afbc59a73e2f909047cfd0aaaf6ffb18b2531
b1urrrr <b1urrr@naver.com>
2023-04-20 02:44:36 +0900

    [FEAT/#1] 주사위 굴리기 로직 구현 및 빌드

--- week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-import
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -30,27 +32,40 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage(Modifier)
+    DiceWithButtonAndImage(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+    )
 }
 
 @Composable
-fun DiceWithButtonAndImage(
-    modifier: Modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center),
-) {
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    val diceResult = remember { mutableStateOf(1) }
+    val imageResource = when (diceResult.value) {
+        1 -> R.drawable.img_main_dice_1
+        2 -> R.drawable.img_main_dice_2
+        3 -> R.drawable.img_main_dice_3
+        4 -> R.drawable.img_main_dice_4
+        5 -> R.drawable.img_main_dice_5
+        else -> R.drawable.img_main_dice_6
+    }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Image(
-            painter = painterResource(id = R.drawable.img_main_dice_1),
-            contentDescription = stringResource(R.string.main_dice_img_content_description),
+            painter = painterResource(id = imageResource),
+            contentDescription = stringResource(
+                R.string.main_dice_img_content_description,
+                diceResult.value,
+            ),
         )
         Spacer(
             modifier = Modifier.height(16.dp),
         )
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { diceResult.value = (1..6).random() }) {
             Text(text = stringResource(R.string.main_roll))
         }
     }
