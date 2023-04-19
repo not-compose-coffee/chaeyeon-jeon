5c3455ebbb7428c5501e7a5334cafaf6d2042d31
b1urrrr <b1urrr@naver.com>
2023-04-20 02:17:48 +0900

    [ADD/#1] 주사위 이미지 컴포저블 추가

--- week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +43,13 @@ fun DiceWithButtonAndImage(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_main_dice_1),
+            contentDescription = stringResource(R.string.main_dice_img_content_description),
+        )
+        Spacer(
+            modifier = Modifier.height(16.dp),
+        )
         Button(onClick = { /*TODO*/ }) {
             Text(text = stringResource(R.string.main_roll))
         }
