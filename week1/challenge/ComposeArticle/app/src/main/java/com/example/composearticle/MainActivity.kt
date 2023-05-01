feb8838bc0c1e09e49ceb6e72411e629d84d4c3b
b1urrrr <b1urrr@naver.com>
2023-05-02 01:59:49 +0900

    [MOVE/#1] challenge 디렉토리 생성

--- week1/challenge/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ week1/challenge/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,23 +3,11 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +17,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+}
 
 @Composable
 private fun ArticleCard(
@@ -42,7 +31,7 @@ private fun ArticleCard(
     Column() { }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+}
