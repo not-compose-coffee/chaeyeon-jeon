2b765c401b59857a640e31f18812fcd92803f17e
b1urrrr <b1urrr@naver.com>
2023-05-02 16:05:39 +0900

    [FEAT/#2] ComposeQuadrant 레이아웃 구현

--- week1/challenge/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ week1/challenge/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -17,20 +15,51 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeQuadrantApp()
+        }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun ComposeQuadrantApp() {
-    Column() {
-        Row() { }
-        Row() { }
+fun ComposeQuadrantApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+    ) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.main_text_composable_title),
+                description = stringResource(R.string.main_text_composable_description),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f),
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.main_image_composable_title),
+                description = stringResource(R.string.main_image_composable_description),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f),
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.main_row_composable_title),
+                description = stringResource(R.string.main_row_composable_description),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f),
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.main_column_composable_title),
+                description = stringResource(R.string.main_column_composable_description),
+                backgroundColor = Color.LightGray,
+                modifier = Modifier.weight(1f),
+            )
+        }
     }
 }
 
@@ -39,12 +68,25 @@ private fun ComposableInfoCard(
     title: String,
     description: String,
     backgroundColor: Color,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    Column( ) { }
+    Column(
+        modifier = modifier
+            .background(backgroundColor)
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(alignment = CenterHorizontally)
+                .padding(bottom = 16.dp),
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+        )
+    }
 }
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() { }
